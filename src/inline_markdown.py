1c58d5b0801ea8a9541f704e163ee786c73a76ae
ashraddhansh <ashraddhansh@protonmail.com>
2026-01-23 23:26:47 +0530

    slight changes in inline markdown

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -1,23 +1,27 @@
 from textnode import TextNode, TextType
-
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
     new_nodes = []
     for node in old_nodes:
         if node.text_type != TextType.TEXT:
             new_nodes.append(node)
             continue
+        if node.text.count(delimiter) %2 != 0:
+            raise Exception("Invalid Markdown Syntax: formatted section not closed")
 
-        if node.text.count(delimiter) % 2 != 0:
-            raise ValueError("Invalid markdown: formatted section not closed")
-
-        parts = node.text.split(delimiter)
-        for i, part in enumerate(parts):
-            if part == "":
+        parts = []
+        parts.extend(node.text.split(delimiter))
+        split_nodes = []
+        for i in range(len(parts)):
+            if parts[i] == "":
                 continue
-            if i % 2 == 0:
-                new_nodes.append(TextNode(part, TextType.TEXT))
+            if i%2 == 0:
+                split_nodes.append(TextNode(parts[i], TextType.TEXT))
             else:
-                new_nodes.append(TextNode(part, text_type))
-
+                split_nodes.append(TextNode(parts[i], text_type))
+        new_nodes.extend(split_nodes)
     return new_nodes
+        
+
+
+
 
