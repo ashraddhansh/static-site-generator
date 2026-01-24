c2daf4770db8bf901a24e82479ec78e2b515e949
ashraddhansh <ashraddhansh@protonmail.com>
2026-01-25 00:48:37 +0530

    block to block_type and it's tests

--- src/block_markdown.py
+++ src/block_markdown.py
@@ -1,5 +1,81 @@
+from enum import Enum
+
 def markdown_to_blocks(markdown):
     blocks = markdown.split("\n\n")
     clean = list(filter(lambda x: x is not None and x.strip() != "", blocks))
     return list(map(lambda x: x.strip(), clean))
+
+class BlockType(Enum):
+    PARAGRAPH = "paragraph"
+    HEADING = "heading"
+    CODE = "code"
+    QUOTE = "quote"
+    ULIST = "unordered list"
+    OLIST = "ordered list"
+
+def block_to_block_type(markdown_text):
+    def is_code_block(text):
+        return text.startswith("```\n") and text.endswith("```")
+    def is_quote_block(text):
+        parts = text.split("\n")
+        for part in parts:
+            if not part.startswith("> "):
+                return False
+        return True
+    def is_u_list(text):
+        parts = text.split("\n")
+        for part in parts:
+            if not part.startswith("- "):
+                return False
+        return True
+
+    def is_o_list(text):
+        parts = text.split("\n")
+        expected = 1
+        for part in parts:
+            if not part[0].isdigit():
+                return False
+            i = 0
+            while i < len(part) and part[i].isdigit():
+                i +=1
+
+            if i+1 >= len(part):
+                return False
+            if part[i] != "." or part[i+1] != " ":
+                return False
+            num = int(part[:i])
+            if num != expected:
+                return False
+
+            expected += 1
+        return True
+
+
+    def is_heading(text):
+        if not text.startswith("#"):
+            return False
+        i = 0
+        while i < len(text) and text[i] == "#":
+            i += 1
+
+        if i > 6:
+            return False
+        if i == len(text):
+            return False
+        if text[i] != " ":
+            return False
+        return True
+       
+    returned = BlockType.PARAGRAPH
+    if is_heading(markdown_text):
+        returned = BlockType.HEADING
+    if is_quote_block(markdown_text):
+        returned = BlockType.QUOTE
+    if is_code_block(markdown_text):
+        returned = BlockType.CODE
+    if is_o_list(markdown_text):
+        returned = BlockType.OLIST
+    if is_u_list(markdown_text):
+        returned = BlockType.ULIST
+    return returned
 
--- src/test_block_markdown.py
+++ src/test_block_markdown.py
@@ -1,5 +1,5 @@
 import unittest
-from block_markdown import markdown_to_blocks
+from block_markdown import markdown_to_blocks, block_to_block_type, BlockType
 
 
 class TestBlockMarkdown(unittest.TestCase):
@@ -45,5 +45,98 @@ This is the same paragraph on a new line
                     "- This is a list\n- with items",
                 ],
             )
+        
+        def test_block_to_block_type_paragraph(self):
+            text = "This is Arpit"
+            type = block_to_block_type(text)
+            self.assertEqual(type,BlockType.PARAGRAPH)
 
+            text = "- This is Arpit"
+            type = block_to_block_type(text)
+            self.assertNotEqual(type,BlockType.PARAGRAPH)
+
+            text = "1. This is Arpit"
+            type = block_to_block_type(text)
+            self.assertNotEqual(type,BlockType.PARAGRAPH)
+
+        def test_block_to_block_type_heading1(self):
+            text = "# Heading 1"
+            type = block_to_block_type(text)
+            self.assertEqual(type,BlockType.HEADING)
+
+            text = "Heading 1"
+            type = block_to_block_type(text)
+            self.assertNotEqual(type,BlockType.HEADING)
+
+            text = "#Heading 1"
+            type = block_to_block_type(text)
+            self.assertNotEqual(type,BlockType.HEADING)
+
+            text = "####### Heading 1"
+            type = block_to_block_type(text)
+            self.assertNotEqual(type,BlockType.HEADING)
+
+            text = "##"
+            type = block_to_block_type(text)
+            self.assertNotEqual(type,BlockType.HEADING)
+
+        def test_block_to_block_type_code(self):
+            text = "```\nprint('hello')```"
+            type = block_to_block_type(text)
+            self.assertEqual(type,BlockType.CODE)
+
+            text = "```\nprint('hello')\n```"
+            type = block_to_block_type(text)
+            self.assertEqual(type,BlockType.CODE)
+
+            text = "``\nprint('hello')```"
+            type = block_to_block_type(text)
+            self.assertNotEqual(type,BlockType.CODE)
+
+            text = "```\nprint('hello')"
+            type = block_to_block_type(text)
+            self.assertNotEqual(type,BlockType.CODE)
+
+        def test_block_to_block_type_quote(self):
+            text = "> Hello this is quote1\n> this is quote 2"
+            type = block_to_block_type(text)
+            self.assertEqual(type,BlockType.QUOTE)
+
+            text = "> Hello this is quote1\n>this is quote 2"
+            type = block_to_block_type(text)
+            self.assertNotEqual(type,BlockType.QUOTE)
+
+        def test_block_to_block_type_olist(self):
+            text = "1. hello this is arpit\n2. Hello this is again me"
+            type = block_to_block_type(text)
+            self.assertEqual(type, BlockType.OLIST)
+
+            text = "1. hello this is arpit\n2. Hello this is again me"
+            type = block_to_block_type(text)
+            self.assertEqual(type, BlockType.OLIST)
+
+            text = "1. hello this is arpit\n3. Hello this is again me"
+            type = block_to_block_type(text)
+            self.assertNotEqual(type, BlockType.OLIST)
+
+            text = "2. hello this is arpit\n3. Hello this is again me"
+            type = block_to_block_type(text)
+            self.assertNotEqual(type, BlockType.OLIST)
+
+            text = "1.hello this is arpit\n2. Hello this is again me"
+            type = block_to_block_type(text)
+            self.assertNotEqual(type, BlockType.OLIST)
+
+        def test_block_to_block_type_ulist(self):
+            text = "- first element\n- second element"
+            type = block_to_block_type(text)
+            self.assertEqual(type, BlockType.ULIST)
+
+            text = "-first element\n- second element"
+            type = block_to_block_type(text)
+            self.assertNotEqual(type, BlockType.ULIST)
+
+            text = "- first element\n-second element"
+            type = block_to_block_type(text)
+            self.assertNotEqual(type, BlockType.ULIST)
 
